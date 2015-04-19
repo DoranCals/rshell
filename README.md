55917dd1cbc1f57d4c693a5d53915a8a6e51b10d
Doran Cals <dcals001@ucr.edu>
2015-04-19 11:16:02 -0700

    Added connectors, updated documentation

--- README.md
+++ README.md
@@ -2,26 +2,21 @@
 ### a simple command shell
 
 Rshell executes commands read from standard input.
-It is not a complete shell (yet) as it does not allow the working directory to be changed.
-
-Type any command, and Rshell will execute the corresponding program in $PATH.
+Type any command, and Rshell will execute the corresponding program.
 Type "exit" to quit the shell.
 
-#### This stuff does not actually work yet!
 Enter multiple commands on a single line by using these connectors:
 && executes the right side if the left side succeeds.
 || executes the right side if the left side fails.
 ; always executes the right side.
 # never executes the right side (thus making it a comment).
 
-If a line starts with a connector, the exit status from the previous line will be used.
-If a line ends with a connector, the connector will be ignored.
+Connectors at the start of a line will proceed as if the left side succeeded.
+Connectors at the end of a line will be ignored.
 If multiple connectors are adjacent to each other, undefined behavior will result.
 Connectors are evaluated left to right.
 
-
 ###KNOWN ISSUES
-The shell currently does not accept connectors or comments.
-The shell may freak out when a command is entered and not execute it correctly.
-Ctrl-D will create an infinite output loop.
+The shell will freak out on invalid commands, arguments, or connectors. (A memory leak would occur otherwise.)
+Ctrl-D will exit the shell with an error.
 
--- src/main.cpp
+++ src/main.cpp
@@ -14,29 +14,34 @@
 using namespace std;
 using namespace boost;
 
-typedef tokenizer<char_separator<char> > tokInput;
+//typedef tokenizer<char_separator<char> > tokInput;
 
 // Runs a process with arguments.
-// The process name is specified by the first space-separated word in the string.
-// Returns zero on success, non-zero on failure
+// The process name is specified by the first space-separated word.
+// Returns the process's exit status, or 1 if the process didn't start.
 int runProcess(string lineInput)
 {
 	char_separator<char> spaces(" \t\n\r");
 	tokenizer<char_separator<char> > tokCommand(lineInput, spaces);
-	if (*(tokCommand.begin() ) == "exit") exit(0); // quit immediately if "exit" is invoked
 
-	int argc = 0; // How many arguments are there? Tokenizer will find out
+	if (tokCommand.begin() == tokCommand.end() ) return 0; // do nothing
+
+	if (*(tokCommand.begin() ) == "exit") exit(0); // quit rshell immediately
+
+	int argc = 1; // leave space for the null pointer
 	for (auto i = tokCommand.begin(); i != tokCommand.end(); i++)
 	{
 		argc++;
 	}
-	char** argv = new char*[argc+1];
+
+	char** argv = new char*[argc];
 	{
 		int j = 0;
 		for (auto i = tokCommand.begin(); i != tokCommand.end(); i++)
 		{
 			argv[j] = new char[i->size()+1];
 			argv[j] = (char*)i->c_str();
+			argv[j][i->size()] = 0;
 			j++;
 		}
 		argv[j] = 0;
@@ -50,15 +55,17 @@ int runProcess(string lineInput)
 	}
 	else if (f == 0) // child process
 	{
-		cout << "Executing..." << endl;
 		execvp(argv[0], argv);
-		cout << "Couldn't execute." << endl;
-		// if execvp() didn't work, deallocate the c-string arguments passed into it
-		for (int i = 0; i < argc+1; i++)
+		// if execvp() didn't work, deallocate the c-strings passed into it
+		for (int i = 0; i < argc; i++)
 		{
-			delete argv[i];
+			//cout << "Deleting argv[" << i << "]... " << flush; // TESTING
+			delete [] argv[i];
+			//cout << "done." << endl; // TESTING
 		}
-		delete argv;
+		//cout << "Deleting argv... " << endl; // TESTING
+		delete [] argv;
+		//cout << "done."; // TESTING
 		perror("execvp");
 		return 1;
 	}
@@ -88,36 +95,49 @@ int main(int argc, char** argv)
 			cout << getlogin() << "@" << hostname;
 		cout << "$ ";
 
-		vector<string> commands;
+		string thisCommand = "";
 
 		getline(cin, lineInput);
+		lineInput += " ;";
 
 		tokenizer<char_separator<char> > tokInput(lineInput, connectors);
 
 		auto i = tokInput.begin();
 		while (i != tokInput.end() )
 		{
-			// Will search for connectors and reformat the commands based on those
-			auto j = i;
-
-			/*
-			if ( (*i == "&" and j != tokInput.end() and *j == "&")
-				or (*i == "|" and j != tokInput.end() and *j == "|")
-				or *i == ";" or *i == "#" )
+			// If a connector is found, execute the stored command.
+			if (*i == "&" or *i == "|" or *i == ";" or *i == "#" )
 			{
-				
+				prevStatus = runProcess(thisCommand);
+				thisCommand = "";
+				if (*i == "&" or *i == "|")
+				{
+					auto j = i; j++;
+					if (j == tokInput.end() ) break;
+					if (*j == *i)
+					{
+						if (*i == "|")
+						{
+							i++;
+							if (prevStatus == 0) break;
+						}
+						if (*i == "&")
+						{
+							i++;
+							if (prevStatus != 0) break;
+						}
+					}
+				}
+				else if (*i == "#") break; // ignore the rest of the line
+				// Semicolons will be handled automatically;
+				// the token iterator simply moves on to the next command.
 			}
-			else
+			else // Not a connector, so treat it as a command or argument.
 			{
-
+				thisCommand += *i;
+				thisCommand += " ";
 			}
-			*/
 			i++;
 		}
-		// This should go in the while loop above, so it'll run multiple times with connectors
-		prevStatus = runProcess(lineInput); 
-
-		cout << prevStatus << endl; // TESTING
 	}
-
 }
